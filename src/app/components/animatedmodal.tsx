1fa92b888728c172a21936c84e673a0416cd4557
Mikhan-TMP <mikhanbalbastro@outlook.com>
2025-09-07 19:14:57 +0800

    problems in main.tsx

--- src/app/components/animatedmodal.tsx
+++ src/app/components/animatedmodal.tsx
@@ -222,24 +222,24 @@ return (
 // Hook to detect clicks outside of a component.
 // Add it in a separate file, I've added here for simplicity
 export const useOutsideClick = (
-ref: React.RefObject<HTMLDivElement | null>,
-callback: (...args: any[]) => any
+  ref: React.RefObject<HTMLDivElement | null>,
+  callback: (event: MouseEvent | TouchEvent) => void
 ) => {
-useEffect(() => {
-    const listener = (event: any) => {
-    // DO NOTHING if the element being clicked is the target element or their children
-    if (!ref.current || ref.current.contains(event.target)) {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      // DO NOTHING if the element being clicked is the target element or their children
+      if (!ref.current || ref.current.contains(event.target as Node)) {
         return;
-    }
-    callback(event);
+      }
+      callback(event);
     };
 
     document.addEventListener("mousedown", listener);
     document.addEventListener("touchstart", listener);
 
     return () => {
-    document.removeEventListener("mousedown", listener);
-    document.removeEventListener("touchstart", listener);
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
     };
-}, [ref, callback]);
+  }, [ref, callback]);
 };
